87b95fb9dcb642fed151b7a996d50d6bbb824d54
Pritish Sai <Pritish.sai@gmail.com>
2015-12-04 23:05:11 +0530

    Chennai Floods Directory

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204141026) do
+ActiveRecord::Schema.define(version: 20151204172854) do
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "location"
