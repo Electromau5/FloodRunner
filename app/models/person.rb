4e530d4b578a456484edce0fb67be0f3aaf8c565
Pritish Sai <Pritish.sai@gmail.com>
2015-12-05 10:39:12 +0530

    Chennai Floods Directory

--- app/models/person.rb
+++
@@ -1,4 +0,0 @@
-class Person < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "500x500>", thumb: "100x100>" }, default_url: "test1.jpg"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-end
